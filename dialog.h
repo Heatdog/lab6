2646335cd0c9909afb5c3578506550af855573e4
Heatdog <71334663+Heatdog@users.noreply.github.com>
2021-05-25 01:30:38 +0300

    Create dialog.h

---
+++ dialog.h
@@ -0,0 +1,27 @@
+//
+// Created by yura on 24.05.2021.
+//
+
+#ifndef LAB6_DIALOG_H
+#define LAB6_DIALOG_H
+
+#ifndef GRAPH_DIALOG_H
+#define GRAPH_DIALOG_H
+
+#include "list.h"
+
+
+void dialog(Graph *);
+void D_Add(Graph *);
+void D_Show(Graph*);
+void D_Find(Graph*);
+void D_Delete(Graph*);
+void D_Dexter(Graph*);
+void D_Decomposition(Graph*);
+void D_File(Graph*);
+void scan_int(int *);
+char* scan_string(char *);
+
+#endif //GRAPH_DIALOG_H
+
+#endif //LAB6_DIALOG_H
